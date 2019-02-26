665f3f4b2e66c55b081ab188bc77a84d291d510b
andres97 <anflo.97@gmail.com>
2019-02-26 06:46:27 -0500

    Server y Maquina Virtual

---
+++ AWS-VIRTUAL/src/main/java/org/edu/eci/aws/URLReader.java
@@ -0,0 +1,19 @@
+package org.edu.eci.aws;
+import java.io.*; 
+import java.net.*; 
+import java.util.*;
+
+public class URLReader { 
+  public static void main(String[] args) throws Exception { 
+      URL url = new URL(args[0]); 
+      try (BufferedReader reader = new BufferedReader(
+          new InputStreamReader(url.openStream()))) { 
+            String inputLine = null; 
+            while ((inputLine = reader.readLine()) != null) { 
+                  System.out.println(inputLine); 
+             } 
+       } catch (IOException x) { 
+               System.err.println(x); 
+       } 
+    } 
+}
